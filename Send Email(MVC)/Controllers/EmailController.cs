188e6771d1f7366a08d8db059ce0984f63931574
SurajDotNetDeveloper <surajcool9623@gmail.com>
2024-08-22 03:09:04 +0530

    wdf

---
+++ Send Email(MVC)/Controllers/EmailController.cs
@@ -0,0 +1,56 @@
+﻿using Send_Email_MVC_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Send_Email_MVC_.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendEmail(EmailModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    SmtpClient client = new SmtpClient("smtp.gmail.com")
+                    {
+                        Port = 587, // Use 465 for SSL or 587 for TLS
+                        Credentials = new NetworkCredential("itsganeshpawar99@gmail.com", "dgvw fsaz snua elga"),
+                        EnableSsl = true, // Use SSL/TLS
+                    };
+
+                    MailMessage mailMessage = new MailMessage
+                    {
+                        From = new MailAddress("itsganeshpawar99@gmail.com"),
+                        Subject = model.Subject,
+                        Body = model.Body,
+                        IsBodyHtml = false,
+                    };
+
+                    mailMessage.To.Add(model.To);
+
+                    client.Send(mailMessage);
+                    ViewBag.Message = "Email successfully sent!";
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = $"Error: {ex.Message}";
+                }
+            }
+
+            return View("Index", model);
+        }
+    }
+}
+    
---
+++ Send Email(MVC)/Models/EmailModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Send_Email_MVC_.Models
+{
+    public class EmailModel
+    {
+        [Required, Display(Name = "Recipient Email"), EmailAddress]
+        public string To { get; set; }
+
+        [Required, Display(Name = "Subject")]
+        public string Subject { get; set; }
+
+        [Required, Display(Name = "Message")]
+        public string Body { get; set; }
+    }
+}
